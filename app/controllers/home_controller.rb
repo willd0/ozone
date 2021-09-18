23277cb15b13c900d8b32716458a6241a97f0bf2
Will <willd2122@gmail.com>
2021-09-18 17:53:05 -0500

    added change marker color on dragend

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,38 +1,4 @@
 class HomeController < ApplicationController
-  def index
-    require 'net/http'
-    require 'json'
-    require '/railsfriends/ozone/.api_key.rb'
-    @url = 'https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=39.0509&longitude=-121.4453&date=2021-09-14&distance=25&API_KEY='+ $api_key
-    @uri = URI(@url)
-    @response = Net::HTTP.get(@uri)
-    @output = JSON.parse(@response)
-    
-    #Check for empty return result
-    if @output.empty?
-      @final_output = "Error"
-    elsif !@output
-      @final_output = "Error"
-    else
-      @final_output = @output[0]["AQI"]
-    end
-
-    if @final_output == "Error"
-      @api_color = "silver"
-    elsif @final_output <= 50
-      @api_color = "green"
-    elsif @final_output >= 51 && @final_output <= 100
-      @api_color = "yellow"
-    elsif @final_output >= 101 && @final_output <= 150
-      @api_color = "orange"
-    elsif @final_output >= 151 && @final_output <= 200
-      @api_color = "red"
-    elsif @final_output >= 201 && @final_output <= 300
-      @api_color = "purple"
-    elsif @final_output >= 301 && @final_output <= 500
-      @api_color = "maroon"
-    end
-      
-      
+  def index     
   end
 end
